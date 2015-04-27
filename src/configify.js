31cdcf28cd7bf404c067a04d6e628a0be67ff5fe
Adnan Hajdarevic <adnanh@gmail.com>
2015-04-27 14:47:23 +0200

    init

---
+++ src/configify.js
@@ -0,0 +1,46 @@
+var tools = require('browserify-transform-tools');
+
+// Transform function
+function transform(content, transformOptions, done) {
+    var file = transformOptions.file;
+
+    transformOptions.config.files = transformOptions.config.files || ['config.json'];
+    transformOptions.config.environment = transformOptions.config.environment || 'development';
+
+    var shouldTransform = false;
+    for (var i = 0; i < transformOptions.config.files.length; i++) {
+        var rxp = new RegExp(transformOptions.config.files[i] + '$', 'gi');
+
+        if (file.match(rxp)) {
+            shouldTransform = true;
+            break;
+        }
+    }
+
+    if (shouldTransform) {
+        var rawConfigObject = JSON.parse(content);
+
+        var configObject = {};
+
+        if (rawConfigObject.hasOwnProperty('options')) {
+            for (var key in rawConfigObject.options) {
+                configObject[key] = rawConfigObject.options[key];
+            }
+        }
+
+        if (rawConfigObject.hasOwnProperty(transformOptions.config.environment)) {
+            for (var key in rawConfigObject[transformOptions.config.environment]) {
+                configObject[key] = rawConfigObject[transformOptions.config.environment][key];
+            }
+        }
+
+        var newContent = "var config = " + JSON.stringify(configObject) + ";\nmodule.exports = config;";
+
+        return done(null, newContent);
+    } else {
+        return done(null, content);
+    }
+}
+
+// Export transform
+module.exports = tools.makeStringTransform('configify', { includeExtensions: [".json"] }, transform);
